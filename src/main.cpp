596846d937881120219e285bf587e07069ca802d
Zack Allen - Laptop <zaal3595@colorado.edu>
2025-12-06 14:01:24 -0700

    big behavior tree for the humanoid

--- src/main.cpp
+++ src/main.cpp
@@ -7,55 +7,78 @@
 int main() {
     BT::BehaviorTreeFactory factory;
 
-    factory.registerSimpleAction("A", [](BT::TreeNode&) { return BT::NodeStatus::SUCCESS; });
-    factory.registerSimpleAction("B", [](BT::TreeNode&) { return BT::NodeStatus::SUCCESS; });
-    factory.registerSimpleAction("C", [](BT::TreeNode&) { return BT::NodeStatus::SUCCESS; });
+    factory.registerSimpleAction("Already_Unscrewed_Bolt", [](BT::TreeNode&) { return BT::NodeStatus::SUCCESS; });
+    factory.registerSimpleAction("Humanoid_Not_Removing_Bolt", [](BT::TreeNode&) { return BT::NodeStatus::SUCCESS; });
+    factory.registerSimpleAction("At_Bolt_Location", [](BT::TreeNode&) { return BT::NodeStatus::SUCCESS; });
+    factory.registerSimpleAction("Go_To_Bolt_Location", [](BT::TreeNode&) { return BT::NodeStatus::SUCCESS; });
+    factory.registerSimpleAction("Unscrew_Bolt", [](BT::TreeNode&) { return BT::NodeStatus::SUCCESS; });
 
-
-
-    // auto tree = factory.createTreeFromText(R"(
-    // <root main_tree_to_execute="MainTree" BTCPP_format="4">
-    //     <BehaviorTree ID="MainTree">
-    //         <A/>
-    //     </BehaviorTree>
+    // auto tree = factory.createTreeFromText
+    // (R"(
+    // <root
+    // main_tree_to_execute="MainTree"
+    // BTCPP_format="4">
+    // <BehaviorTree ID="MainTree">
+    //     <Sequence
+    //     name="root_sequence">
+    //         <Fallback name="Remove_Bolt_1">
+    //             <Bolt_Already_Removed/>
+    //             <Sequence name="Do_Remove_Bolt_1">
+    //                 <Arm_Not_at_Bolt/>
+    //                 <Remove_the_Bolt/>
+    //             </Sequence>
+    //         </Fallback>
+    //         <Fallback name="Remove_Bolt_2">
+    //             <Bolt_Already_Removed/>
+    //             <Sequence name="Do_Remove_Bolt_2">
+    //                 <Arm_Not_at_Bolt/>
+    //                 <Remove_the_Bolt/>
+    //             </Sequence>
+    //         </Fallback>
+    //         <Fallback name="Remove_Bolt_3">
+    //             <Bolt_Already_Removed/>
+    //             <Sequence name="Do_Remove_Bolt_3">
+    //                 <Arm_Not_at_Bolt/>
+    //                 <Remove_the_Bolt/>
+    //             </Sequence>
+    //         </Fallback>
+    //     </Sequence>
+    // </BehaviorTree>
     // </root>
     // )");
-    auto tree = factory.createTreeFromText(R"(
-        <root main_tree_to_execute="MainTree" BTCPP_format="4">
-            <BehaviorTree ID="MainTree">
-                <Sequence name="root_sequence">
-                    <A/>
-                    <B/>
-                </Sequence>
-            </BehaviorTree>
+
+    auto tree = factory.createTreeFromText
+        (R"(
+        <root
+        main_tree_to_execute="MainTree"
+        BTCPP_format="4">
+        <BehaviorTree ID="MainTree">
+            <Fallback
+            name="root_fallback">
+            <Already_Unscrewed_Bolt/>
+            <Sequence name="sq1">
+                <Humanoid_Not_Removing_Bolt/>
+                <Fallback name="fb">
+                    <Go_To_Bolt_Location/>
+                    <Unscrew_Bolt/>
+                </Fallback>
+                <Unscrew_Bolt/>
+            </Sequence>
+            </Fallback>
+        </BehaviorTree>
         </root>
-    )");
-//     auto tree = factory.createTreeFromText(R"(
-//     <root main_tree_to_execute="MainTree" BTCPP_format="4">
-//         <BehaviorTree ID="MainTree">
-//             <Fallback name="root_fallback">
-//                 <A/>
-//                 <B/>
-//             </Fallback>
-//         </BehaviorTree>
-//     </root>
-// )");
+        )");
 
     BTtoBuchiConverter converter;
     auto start = converter.toBuchi(tree.rootNode());
     std::cout << start->name_ << std::endl;
     auto next = start->transition(AP::FAILURE);
-    // next = next->transition(AP::FAILURE);
     std::cout << next->name_ << std::endl;
     next = next->transition(AP::SUCCESS);
     std::cout << next->name_ << std::endl;
     next = next->transition(AP::FAILURE);
     std::cout << next->name_ << std::endl;
-    next = next->transition(AP::SUCCESS);
-    next = next->transition(AP::SUCCESS);
+    next = next->transition(AP::FAILURE);
     std::cout << next->name_ << std::endl;
-
-
-
     return 0;
 }
